a989ae3217f0194e022ad455112c13f2446dc06e
Haekyu Park <hkpark627@gmail.com>
2019-10-21 15:14:22 -0700

    I_matrix

--- code/I_matrix.py
+++ code/I_matrix.py
@@ -16,180 +16,6 @@ import lucid.modelzoo.vision_models as models
 from keras.applications.inception_v3 import preprocess_input
 from data_parser import _parse_function
 
-'''
-Main function
-'''
-def main():
-    '''
-    Parse the arguments
-    '''
-    args = parse_args()
-    layer = args.layer
-    gpu = args.gpu
-    batch = args.batch
-
-    os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu)
-
-    googlenet = models.InceptionV1()
-    googlenet.load_graphdef()
-    nodes = googlenet.graph_def.node
-
-    # filenames = glob.glob('/media/fred/strawberry/imagenet-tf-records/*')
-    # filenames = glob.glob('test-images/imagenet-tf-records/*')
-    filenames = glob.glob('/raid/hpark407/imagenet-tf-records/*')
-    I_mat_dirpath = '/raid/fhohman3/I-mat/'
-    # chain_dirpath = './chain/'
-
-    num_class = 1000
-    all_layers = get_layers(nodes)
-    mixed_layers = [layer for layer in all_layers if 'mixed' in layer]
-
-    layers = {
-        'mixed3a': 256,
-        'mixed3b': 480,
-        'mixed4a': 508,
-        'mixed4b': 512,
-        'mixed4c': 512,
-        'mixed4d': 528,
-        'mixed4e': 832,
-        'mixed5a': 832,
-        'mixed5b': 1024
-    }
-
-    layer_fragment_sizes = {layer: get_channel_sizes(layer, nodes) for layer in mixed_layers}
-    weight_sizes = get_weight_sizes(nodes, all_layers)
-    act_sizes = get_act_sizes(weight_sizes, mixed_layers)
-
-    k = 5
-    # chain_k = 3
-    mixed_layer = layer.split('_')[0]
-    prev_layer = get_prev_layer(all_layers, mixed_layer)
-
-    a_sz = act_sizes[mixed_layer]
-    f_sz = layer_fragment_sizes[mixed_layer]
-
-    frag_sz = [f_sz[0], f_sz[1], f_sz[2], f_sz[3], a_sz[1], a_sz[2]]
-
-    outlier_nodes = ['mixed3a-67', 'mixed3a-190', 'mixed3b-390', 'mixed3b-399', 'mixed3b-412']
-    outlier_nodes_idx = [int(n.split('-')[1]) for n in outlier_nodes if layer in n]
-
-    # Get top impactful previous neurons and generate I-matrices
-    # Get layer info
-    is_mixed = '_' not in layer
-    branch = None if is_mixed else int(layer.split('_')[-1])
-
-    # Initialize I
-    num_channel = layers[layer] if is_mixed else act_sizes[layer[:-2]][branch]
-    I_layer = gen_empty_I(num_class, num_channel)
-
-    # Run
-    with tf.Graph().as_default():
-        # Get dataset
-        dataset = tf.data.TFRecordDataset(filenames)
-        dataset = dataset.map(_parse_function)
-        dataset = dataset.map(lambda img, lab, syn: (preprocess_input(img), lab, syn))
-        dataset = dataset.batch(batch)
-
-        # Iterate tf-records
-        iterator = dataset.make_one_shot_iterator()
-        t_preprocessed_images, t_labels, t_synsets = iterator.get_next()
-
-        # Import googlenet
-        T = render.import_model(googlenet, t_preprocessed_images, None)
-
-        # Get weight tensors
-        t_w0, t_w1, t_w2, t_w3, t_w4, t_w5 = get_weight_tensors(mixed_layer)
-
-        # Get intermediate layer tensors
-        t_a0, t_a1, t_a2 = get_intermediate_layer_tensors(prev_layer, mixed_layer)
-
-        # Define intermediate conv output tensors
-        t_inf_0 = get_infs(t_a0, t_w0)
-        t_inf_1 = get_infs(t_a1, t_w2)
-        t_inf_2 = get_infs(t_a2, t_w4)
-        t_inf_3 = get_infs(t_a0, t_w5)
-        t_inf_4 = get_infs(t_a0, t_w1)
-        t_inf_5 = get_infs(t_a0, t_w3)
-
-        # Run with batch
-        progress_counter = 0
-        with tf.Session() as sess:
-            start = time.time()
-
-            try:
-                with tqdm.tqdm(total=1281167, unit='imgs') as pbar:
-
-                    while(True):
-                        progress_counter += 1
-
-                        # Run the session
-                        if is_mixed:
-                            labels, inf_0, inf_1, inf_2, inf_3 = sess.run([t_labels, t_inf_0, t_inf_1, t_inf_2, t_inf_3])
-
-                        elif branch == 1:
-                            labels, inf_4 = sess.run([t_labels, t_inf_4])
-
-                        elif branch == 2:
-                            labels, inf_5 = sess.run([t_labels, t_inf_5])
-
-                        # Add up the counts
-                        if is_mixed:
-                            channel = 0
-                            for frag, inf in enumerate([inf_0, inf_1, inf_2, inf_3]):
-                                channel = update_I(layer, inf, channel, I_layer, labels, frag_sz[frag], k, outlier_nodes_idx)
-
-                        elif branch == 1:
-                            update_I(layer, inf_4, 0, I_layer, labels, frag_sz[4], k, outlier_nodes_idx)
-
-                        elif branch == 2:
-                            update_I(layer, inf_5, 0, I_layer, labels, frag_sz[5], k, outlier_nodes_idx)
-
-                        pbar.update(len(labels))
-                        # print(inf_0.shape, inf_1.shape, inf_2.shape, inf_3.shape, inf_4.shape, inf_5.shape)
-
-            except tf.errors.OutOfRangeError:
-                pass
-
-            # Save I_layer
-            with open(I_mat_dirpath + 'I_%s.json' % layer, 'w') as f:
-                json.dump(I_layer, f, indent=2)
-
-            end = time.time()
-            print(end - start)
-            print(progress_counter)
-            print(progress_counter * batch)
-
-    # Generate chains
-    # pred_class = 270
-    # channels = [1, 120]
-    # generate_save_chain(pred_class, all_layers, I_mat_dirpath, channels)
-
-
-def parse_args():
-    '''
-    Parse arguments and pass as arguments object
-    '''
-
-    parser = argparse.ArgumentParser('Summit')
-
-    parser.add_argument('--layer', type=str, default='mixed3a',
-                        help='name of layer to generate I matrix')
-
-    parser.add_argument('--gpu', type=int, default=0,
-                        help='gpu cuda visible device')
-
-    parser.add_argument('--batch', type=int, default=500,
-                        help='batch size for loading images')
-
-    parser.add_argument('--num_class', type=int, default=1000,
-                        help='the number of classes')
-
-    parser.add_argument('--k', type=int, default=5,
-                        help='TODO: need better name and explanation on this')
-
-    return parser.parse_args()
-
-
 
 def get_weight_tensors(layer):
     '''
@@ -443,7 +269,7 @@ def gen_empty_I(num_class, num_channel):
 
 def update_I(layer, influences, channel, I_layer, labels, num_out_channel, k, outlier_nodes_idx):
 
-    start = time.time()
+    start = time()
     num_in_channel = int(influences.shape[-1]/num_out_channel)
 
     temp = 0
@@ -488,49 +314,6 @@ def load_inf_matrix(mat_dirpath, layer):
         I_mat = json.load(f)
 
     return I_mat
-
-
-# def gen_impactful_chains(Is, layer, channels, pred_class, all_layers, layer_fragment_sizes, chain_k):
-#     '''
-#     Generate impactful chains starting from channels in a layer to input layer
-#     * input
-#         - Is: influence matrices
-#         - layer: starting layer
-#         - channels: selected channels in the layer
-#         - pred_class: predicted class
-#         - all_layers: all layers
-#         - k: the number of top impactful previous channels
-#     * output
-#         - chains: a dictionary, whose
-#             - key: a layer
-#             - val: a dictionary, which maps
-#                 - key: a neuron in current layer
-#                 - val: list of important neurons in the previous layer
-#     '''
-
-#     # Get layers starting from the given layer to the input layer
-#     start_idx, end_idx = all_layers.index(layer), all_layers.index('mixed3a')
-#     target_layers = all_layers[start_idx: end_idx - 1: -1]
-
-#     # Initialize the chains
-#     chains = {}
-
-#     # Aggregate the chains across layers
-#     curr_channels = channels[:]
-#     for curr_layer in target_layers:
-#         chains[curr_layer] = []
-#         agg_prev_channels = []
-
-#         for curr_channel in curr_channels:
-#             prev_channels, prev_infs = get_top_prevs(Is, curr_layer, curr_channel, pred_class, layer_fragment_sizes[curr_layer], chain_k)
-#             curr_channel_dict = {'channel': curr_channel, 'prev_channels': []}
-#             for prev_c, prev_inf in zip(prev_channels, prev_infs):
-#                 curr_channel_dict['prev_channels'].append({'prev_channel': prev_c, 'inf': prev_inf})
-#             chains[curr_layer].append(curr_channel_dict)
-#             agg_prev_channels += prev_channels
-#         curr_channels = agg_prev_channels[:]
-
-#     return chains
 
 
 def get_top_prevs(I_layers, layer, channel, pred_class, layer_channels, k):
@@ -609,6 +392,9 @@ def gen_I_matrix(layer, k, googlenet, batch, input_dir_path, mixed_layers, layer
     # Time checker
     start_time = time()
 
+    is_mixed = '_' not in layer
+
+
     # Get previous layer
     mixed_layer = layer.split('_')[0]
     prev_layer = get_prev_layer(mixed_layers, mixed_layer)
@@ -644,9 +430,6 @@ def gen_I_matrix(layer, k, googlenet, batch, input_dir_path, mixed_layers, layer
 
         # Get intermediate layers' activation tensors
         t_a0, t_a1, t_a2 = get_intermediate_layer_tensors(prev_layer, mixed_layer)
-
-        print(layer)
-        print(t_a0.shape, t_w0.shape)
 
         # Define intermediate depthwise conv output tensors
         t_inf_0 = get_infs(t_a0, t_w0)
@@ -709,7 +492,6 @@ def gen_I_matrix(layer, k, googlenet, batch, input_dir_path, mixed_layers, layer
 
                         pbar.update(len(labels))
 
-                        # print(inf_0.shape, inf_1.shape, inf_2.shape, inf_3.shape, inf_4.shape, inf_5.shape)
 
             except tf.errors.OutOfRangeError:
                 pass
@@ -718,40 +500,10 @@ def gen_I_matrix(layer, k, googlenet, batch, input_dir_path, mixed_layers, layer
             with open(I_mat_dirpath + 'I_%s.json' % layer, 'w') as f:
                 json.dump(I_layer, f, indent=2)
 
-            end = time.time()
+            end = time()
 
             print(end - start)
             print(progress_counter)
             print(progress_counter * batch)
 
     return I_layer
-
-# def generate_save_chain(pred_class, all_layers, I_mat_dirpath, chain_dirpath, channels, layer_fragment_sizes, chain_k):
-#     '''
-#     * input
-#         - channels: starting channels in mixed5b
-#     '''
-#     # Get layers starting from the given layer to the input layer
-#     start_idx, end_idx = all_layers.index('mixed5b'), all_layers.index('mixed3a')
-#     target_layers = all_layers[start_idx: end_idx - 1: -1]
-
-#     # Load I matrices
-#     Is = {}
-#     for layer in target_layers:
-#         print('loading ', layer)
-#         Is[layer] = load_inf_matrix(I_mat_dirpath, layer)
-#         for branch in [1, 2]:
-#             inner_layer = '{}_{}'.format(layer, branch)
-#             Is[inner_layer] = load_inf_matrix(I_mat_dirpath, inner_layer)
-
-#     # Get chains
-#     chains = gen_impactful_chains(Is, 'mixed5b', channels, pred_class, all_layers, layer_fragment_sizes, chain_k)
-
-#     # Save chains
-#     filename = chain_dirpath + 'chain_{}.json'.format(pred_class)
-#     with open(filename, 'w') as f:
-#         json.dump(chains, f, indent=2)
-
-
-if __name__ == '__main__':
-    main()
