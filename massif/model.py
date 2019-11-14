6e658ff3982a2ba48caae04022c798b707fc12ca
Nilaksh Das <nilakshdas@gatech.edu>
2019-11-14 11:45:10 -0500

    Add model configuration

--- massif/model.py
+++ massif/model.py
@@ -7,6 +7,77 @@ class InceptionV1Model(CleverhansModel):
     SCOPE = 'inceptionv1'
     SOFTMAX_OP = 'softmax2'
 
+    IMG_SIZE = 224
+    NUM_CLASSES = 1000
+    LAYER_SIZES = {
+        'mixed3a': 256,
+        'mixed3b': 480,
+        'mixed4a': 508,
+        'mixed4b': 512,
+        'mixed4c': 512,
+        'mixed4d': 528,
+        'mixed4e': 832,
+        'mixed5a': 832,
+        'mixed5b': 1024}
+    LAYER_BLK_SIZES = {
+        # mixed 3b
+        'mixed3b_0': 128,
+        'mixed3b_1': 192,
+        'mixed3b_2': 96,
+        'mixed3b_3': 64,
+        'mixed3b_4': 128,
+        'mixed3b_5': 32,
+        # mixed 4a
+        'mixed4a_0': 192,
+        'mixed4a_1': 208,
+        'mixed4a_2': 48,
+        'mixed4a_3': 64,
+        'mixed4a_4': 96,
+        'mixed4a_5': 16,
+        # mixed 4b
+        'mixed4b_0': 160,
+        'mixed4b_1': 224,
+        'mixed4b_2': 64,
+        'mixed4b_3': 64,
+        'mixed4b_4': 112,
+        'mixed4b_5': 24,
+        # mixed 4c
+        'mixed4c_0': 128,
+        'mixed4c_1': 256,
+        'mixed4c_2': 64,
+        'mixed4c_3': 64,
+        'mixed4c_4': 128,
+        'mixed4c_5': 24,
+        # mixed 4d
+        'mixed4d_0': 112,
+        'mixed4d_1': 288,
+        'mixed4d_2': 64,
+        'mixed4d_3': 64,
+        'mixed4d_4': 144,
+        'mixed4d_5': 32,
+        # mixed 4e
+        'mixed4e_0': 256,
+        'mixed4e_1': 320,
+        'mixed4e_2': 128,
+        'mixed4e_3': 128,
+        'mixed4e_4': 160,
+        'mixed4e_5': 32,
+        # mixed 5a
+        'mixed5a_0': 256,
+        'mixed5a_1': 320,
+        'mixed5a_2': 128,
+        'mixed5a_3': 128,
+        'mixed5a_4': 160,
+        'mixed5a_5': 48,  # 32?
+        # mixed 5b
+        'mixed5b_0': 384,
+        'mixed5b_1': 384,
+        'mixed5b_2': 128,
+        'mixed5b_3': 128,
+        'mixed5b_4': 192,
+        'mixed5b_5': 48}
+    LAYERS = list(sorted(LAYER_SIZES.keys()))
+
     def __init__(self):
         super(InceptionV1Model, self).__init__()
         self._model = models.InceptionV1()
