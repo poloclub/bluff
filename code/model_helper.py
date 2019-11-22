d8bd63b323fc54e464224ad30544ed84ac15a6ae
Haekyu Park <hkpark627@gmail.com>
2019-11-22 16:30:10 -0500

    (model_helper) convert tensor names into formal ones

--- code/model_helper.py
+++ code/model_helper.py
@@ -13,8 +13,81 @@ import lucid.optvis.render as render
 
 
 def get_predictions(model, imgs):
+    '''
+    Get classification predictions for imgs.
+    * input
+        - model: the classification model
+        - imgs: the input imgs
+    * output
+        - prediction: the predicted classes for imgs
+        - output: the output vector in the last layer in model
+    '''
     output = get_activation_map(model, imgs, 'output2')
-    return np.argmax(output, 1), output
+    prediction = np.argmax(output, 1)
+    return prediction, output
+
+
+def get_activation_map(model, imgs, layer):
+    '''
+    Get activation map
+    * input
+        - model: the classification model
+        - imgs: the input imgs
+        - layer: the layer name
+    * output
+        - act_map: the activagtion maps in the layer for imgs
+    '''
+
+    with tf.Graph().as_default(), tf.Session():
+        t_input = tf.compat.v1.placeholder(tf.float32, [None, 224, 224, 3])
+        T = render.import_model(model, t_input, None)
+        act_map = T(layer).eval({t_input: imgs})
+
+    return act_map
+
+
+def get_weight_tensors(layer):
+    '''
+    Get weight tensors for the given layer in the inceptionV1 model
+    * input
+        - layer: the name of the layer in string (e.g., 'mixed3a')
+    * output
+        - t_w_1x1: the tensor of {layer}_1x1_w:0
+        - t_w_3x3_b: the tensor of {layer}_3x3_bottleneck_w:0
+        - t_w_3x3: the tensor of {layer}_3x3_w:0
+        - t_w_5x5_b: the tensor of {layer}_5x5_bottleneck_w:0
+        - t_w_5x5: the tensor of {layer}_5x5_w:0
+        - t_w_pool_reduce: the tensor of {layer}_pool_reduce_w:0
+    '''
+
+    # Get weight tensors
+    t_w_1x1 = tf.get_default_graph().get_tensor_by_name('import/%s_1x1_w:0' % layer)
+    t_w_3x3_b = tf.get_default_graph().get_tensor_by_name('import/%s_3x3_bottleneck_w:0' % layer)
+    t_w_3x3 = tf.get_default_graph().get_tensor_by_name('import/%s_3x3_w:0' % layer)
+    t_w_5x5_b = tf.get_default_graph().get_tensor_by_name('import/%s_5x5_bottleneck_w:0' % layer)
+    t_w_5x5 = tf.get_default_graph().get_tensor_by_name('import/%s_5x5_w:0' % layer)
+    t_w_pool_reduce = tf.get_default_graph().get_tensor_by_name('import/%s_pool_reduce_w:0' % layer)
+
+    return t_w_1x1, t_w_3x3_b, t_w_3x3, t_w_5x5_b, t_w_5x5, t_w_pool_reduce
+
+
+def get_intermediate_layer_tensors(prev_layer, layer):
+    '''
+    Get intermediate (branched) layer tensors
+    * input
+        - prev_layer: the previous layer given in string (e.g., 'mixed3a')
+        - layer: the current layer given in string (e.g., 'mixed3b')
+    * output
+        - t_a_input: the tensor of the previous layer
+        - t_a_3x3: the tensor of the first branch (3x3 bottleneck)
+        - t_a_5x5: the tensor for the second branch (5x5 bottleneck)
+    '''
+
+    # Get intermediate layer tensors
+    t_a_input = tf.get_default_graph().get_tensor_by_name('import/%s:0' % prev_layer)
+    t_a_3x3 = tf.get_default_graph().get_tensor_by_name('import/%s_3x3_bottleneck:0' % layer)
+    t_a_5x5 = tf.get_default_graph().get_tensor_by_name('import/%s_5x5_bottleneck:0' % layer)
+    return t_a_input, t_a_3x3, t_a_5x5
 
 
 def get_depthwise_activation_map(args, model, img, layer, neuron):
@@ -29,14 +102,7 @@ def get_depthwise_activation_map(args, model, img, layer, neuron):
     # Get the filter for the corresponding neuron
     # ??
 
-def get_activation_map(model, imgs, layer):
 
-    with tf.Graph().as_default(), tf.Session():
-        t_input = tf.compat.v1.placeholder(tf.float32, [None, 224, 224, 3])
-        T = render.import_model(model, t_input, None)
-        act_map = T(layer).eval({t_input: imgs})
-
-    return act_map
 
 
 def get_all_layers_activation_map(model, imgs, layers):
@@ -65,50 +131,6 @@ def get_all_layers_activation_score(model, imgs, layers):
             act_scores[layer] = act_score
 
     return act_scores
-
-
-def get_weight_tensors(layer):
-    '''
-    Get weight tensors for the given layer in the inceptionV1 model
-    * input
-        - layer: the name of the layer in string (e.g., 'mixed3a')
-    * output
-        - t_w0: the tensor of {layer}_1x1_w:0
-        - t_w1: the tensor of {layer}_3x3_bottleneck_w:0
-        - t_w2: the tensor of {layer}_3x3_w:0
-        - t_w3: the tensor of {layer}_5x5_bottleneck_w:0
-        - t_w4: the tensor of {layer}_5x5_w:0
-        - t_w5: the tensor of {layer}_pool_reduce_w:0
-    '''
-
-    # Get weight tensors
-    t_w0 = tf.get_default_graph().get_tensor_by_name('import/%s_1x1_w:0' % layer)
-    t_w1 = tf.get_default_graph().get_tensor_by_name('import/%s_3x3_bottleneck_w:0' % layer)
-    t_w2 = tf.get_default_graph().get_tensor_by_name('import/%s_3x3_w:0' % layer)
-    t_w3 = tf.get_default_graph().get_tensor_by_name('import/%s_5x5_bottleneck_w:0' % layer)
-    t_w4 = tf.get_default_graph().get_tensor_by_name('import/%s_5x5_w:0' % layer)
-    t_w5 = tf.get_default_graph().get_tensor_by_name('import/%s_pool_reduce_w:0' % layer)
-
-    return t_w0, t_w1, t_w2, t_w3, t_w4, t_w5
-
-
-def get_intermediate_layer_tensors(prev_layer, layer):
-    '''
-    Get intermediate (branched) layer tensors
-    * input
-        - prev_layer: the previous layer given in string (e.g., 'mixed3a')
-        - layer: the current layer given in string (e.g., 'mixed3b')
-    * output
-        - t_a0: the tensor of the previous layer
-        - t_a1: the tensor of the first branch (3x3 bottleneck)
-        - t_a2: the tensor for the second branch (5x5 bottleneck)
-    '''
-
-    # Get intermediate layer tensors
-    t_a0 = tf.get_default_graph().get_tensor_by_name('import/%s:0' % prev_layer)
-    t_a1 = tf.get_default_graph().get_tensor_by_name('import/%s_3x3_bottleneck:0' % layer)
-    t_a2 = tf.get_default_graph().get_tensor_by_name('import/%s_5x5_bottleneck:0' % layer)
-    return t_a0, t_a1, t_a2
 
 
 def get_blk_of_neuron(args, layer, neuron):
